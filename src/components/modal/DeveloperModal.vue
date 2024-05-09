b2e1d96b850b4fe0a0156116df1fb4aff10cdc27
Kateryna Serhiienko <kate.serhiienko@gmail.com>
2024-05-09 21:33:40 +0200

    update: developer component

--- src/components/modal/DeveloperModal.vue
+++ src/components/modal/DeveloperModal.vue
@@ -1,9 +1,47 @@
 <template>
-  <p>DeveloperModal</p>
+  <ul>
+    <li>Kateryna Serhiienko</li>
+    <li>09599 Freiberg Germany</li>
+    <li>april 2024</li>
+  </ul>
+  <ul>
+    <li
+      v-for="(mediaItem, index) in getMedia"
+      :key="mediaItem.title"
+    >
+      <a
+        :href="mediaItem.href"
+        :title="mediaItem.title"
+        target="_blank"
+      >
+        <svg :viewBox="svg[`media-item-${index + 1}`].viewBox">
+          <path :d="svg[`media-item-${index + 1}`].path" />
+        </svg>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
+  import svgJSON from '../../assets/svg/svg.json';
+
   export default {
     name: 'DeveloperModal',
+    computed: {
+      ...mapGetters(['getMedia']),
+      svg() {
+        return svgJSON;
+      },
+    },
   };
 </script>
+
+<style
+  scoped
+  lang="scss"
+>
+  svg {
+    width: 60px;
+  }
+</style>
